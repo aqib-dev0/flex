a31beaaffe840025c00e94585106d8affd604cbb
Aqib Maqbool <aqib.dev0@gmail.com>
2025-09-24 14:53:02 +0800

    remove dist

--- backend/dist/properties/properties.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { PropertiesService } from './properties.service';
-export declare class PropertiesController {
-    private readonly propertiesService;
-    constructor(propertiesService: PropertiesService);
-    getAllProperties(): Promise<any[]>;
-    getPropertyById(id: string): Promise<any>;
-}
